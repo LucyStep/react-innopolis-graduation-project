aff7c8d0b39d7f3ae163e735fc18a06e723e8c7d
Lucy <8090429l@gmail.com>
2021-12-17 17:39:14 +0300

    Added login page, but redirect doesn't work

--- my_react_app/src/App.js
+++ my_react_app/src/App.js
@@ -1,20 +1,40 @@
 import MainPage from './pages/MainPage/MainPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import BasketPage from './pages/BasketPage/BasketPage';
-
-import {Routes, Route, Navigate} from 'react-router-dom';
-
+import {authEmail, authPassword} from './authUser.json'
+import {Routes, Route, useLocation} from 'react-router-dom';
+import {useEffect} from "react";
 
 function App() {
+
+  //useLocation для перехода со страницы авторизации на страницу товаров, не работает
+
+  const location = useLocation()
+
+  useEffect(() => {
+    if (localStorage.getItem("credentials")) {
+      const credentials = JSON.parse(localStorage.getItem("credentials"));
+      console.log(authEmail)
+      if (credentials.email === authEmail && credentials.password === authPassword)
+        location.pathname = '/products';
+    }
+    else {
+      localStorage.removeItem("credentials")
+      location.pathname = '/login';
+      console.log(location.pathname)
+    }
+  }, [location])
+
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Navigate to ="/login" />} />
+        <Route path={'/'} element={<LoginPage/>}/>
         <Route path={'products'} element={<MainPage/>}/>
         <Route path={'basket'} element={<BasketPage/>}/>
-        <Route path={'login'} element={<LoginPage/>}/>
       </Routes>
     </div>
+
   )
 }
 
--- my_react_app/src/pages/LoginPage/LoginPage.jsx
+++ my_react_app/src/pages/LoginPage/LoginPage.jsx
@@ -1,19 +1,17 @@
 import './LoginPage.css';
-import {Link} from "react-router-dom";
-import React from "react";
-import Button from "../../components/Button/Button";
-
+import React, {useState} from 'react';
+import Button from '../../components/Button/Button';
 
 function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const emailInput = document.getElementById('input-email');
-  const passwordInput = document.getElementById('input-password');
-  const emailLabel = document.querySelector('.login__label-email');
-  const passwordLabel = document.querySelector('.login__label-password');
-  const emailRequiredSign = document.querySelector('.login__email-wrapper > .login__required-sign');
-  const passwordRequiredSign = document.querySelector('.login__password-wrapper > .login__required-sign');
-  const noEmail = document.querySelector('.login__email-warning');
-  const noPassword = document.querySelector('.login__password-warning');
+  const [isEmailError, setIsEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+
+  const [isPasswordError, setIsPasswordError] = useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+
   const userCredentials = {};
 
   function validateEmail(email) {
@@ -26,108 +24,109 @@ function LoginPage() {
     return re.test(password);
   }
 
-  function makeFieldRed(field = {}) {
-    if (field.email) {
-      noEmail.textContent = field.email;
-      noEmail.style.display = 'block';
-      emailInput.classList.add('invalid-input');
-      emailLabel.classList.add('invalid-text');
-      emailRequiredSign.classList.add('invalid-text');
+// // Проверка имейла
+
+  const emailValidation = (email) => {
+
+    if (email !== "") {
+
+      if (validateEmail(email)) {
+        userCredentials.email = email;
+      } else {
+        setIsEmailError(true);
+        setEmailErrorMessage('Email невалидный')
+      }
     }
-    if (field.password) {
-      noPassword.textContent = field.password;
-      passwordInput.classList.add('invalid-input');
-      passwordLabel.classList.add('invalid-text');
-      passwordRequiredSign.classList.add('invalid-text');
-      noPassword.style.display = 'block';
+
+    if (email === "") {
+      setIsEmailError(true);
+      setEmailErrorMessage('Поле обязательно для заполнения')
     }
   }
 
-// Проверка имейла
+// // Проверка пароля
 
-  const emailValidation = () => {
-    console.log(emailInput)
-    if (emailInput.value !== "") {
+  const passwordValidation = (password) => {
 
-      if (validateEmail(emailInput.value)) {
-        emailInput.classList.remove('invalid-input');
-        emailLabel.classList.remove('invalid-text');
-        emailRequiredSign.classList.remove('invalid-text');
-        noEmail.style.display = 'none';
-        userCredentials.email = emailInput.value;
+    if (password !== "") {
+
+      if (password.length >= 8 && validatePassword(password)) {
+        userCredentials.password = password;
       } else {
-        makeFieldRed({email: 'Email невалидный'})
+        setIsPasswordError(true);
+        setPasswordErrorMessage('Пароль невалидный')
+      }
+      if (password.length < 8) {
+        setIsPasswordError(true);
+        setPasswordErrorMessage('Пароль должен содержать как минимум 8 символов')
       }
     }
 
-    if (emailInput.value === "") {
-      makeFieldRed({email: 'Поле обязательно для заполнения'})
-    }
-  }
-
-// Проверка пароля
-
-  const passwordValidation = () => {
-    if (passwordInput.value !== "") {
-
-      if (passwordInput.value.length >= 8 && validatePassword(passwordInput.value)) {
-        passwordInput.classList.remove('invalid-input');
-        passwordLabel.classList.remove('invalid-text');
-        passwordRequiredSign.classList.remove('invalid-text');
-        noPassword.style.display = 'none';
-        userCredentials.password = passwordInput.value;
-      } else {
-        noPassword.textContent = 'Пароль невалидный';
-      }
-
-      if (passwordInput.value.length < 8) {
-        makeFieldRed({password: 'Пароль должен содержать как минимум 8 символов'})
-      }
-    }
-    if (passwordInput.value === "") {
-      makeFieldRed({password: 'Поле обязательно для заполнения'})
+    if (password === "") {
+      setIsPasswordError(true);
+      setPasswordErrorMessage('Поле обязательно для заполнения')
     }
   }
 
   function onClick() {
-    emailValidation();
-    passwordValidation();
+    passwordValidation(password);
+    emailValidation(email);
     if (userCredentials.email && userCredentials.password) {
-      console.log(userCredentials);
+      localStorage.setItem('credentials', JSON.stringify(userCredentials));
     }
   }
 
   return (
     <div className={'container'}>
-      <form className={'login'} method={'post'}>
+      <form className={'login'}>
         <h1 className={'login__title'}>Вход</h1>
-        <label className={'login__label-email'} htmlFor={'input-email'}>Email</label>
-        <div className={'login__email-wrapper'}>
-          <span className={'login__required-sign'}>*</span>
-          <input className={'login__input'} type={'email'} placeholder="Введите email" id={'input-email'} required/>
+        <label className={'login__label'} htmlFor={'input-email'}>
+          Email
+        </label>
+        <div className={'login__wrapper'}>
+          <input
+            id={'input-email'}
+            value={email}
+            className={'login__input'}
+            type={'email'}
+            placeholder={"Введите email"}
+            required
+            onChange={e => setEmail(e.target.value)}
+          />
         </div>
         <div className={'login__valid'}>
-          <p className={'login__email-warning'}/>
+          {emailErrorMessage}
         </div>
-        <label className={'login__label-password'} htmlFor={'input-password'}>Пароль</label>
-        <div className={'login__password-wrapper'}>
-          <span className={'login__required-sign'}>*</span>
-          <input className={'login__input'} type={'password'} placeholder="Введите пароль" id={'input-password'}
-                 required/>
+        <label className={'login__label'} htmlFor={'input-password'}>
+          Пароль
+        </label>
+        <div className={'login__wrapper'}>
+          <input
+            id={'input-password'}
+            value={password}
+            className={'login__input'}
+            type={'password'}
+            placeholder={"Введите пароль"}
+            required
+            onChange={e => setPassword(e.target.value)}
+          />
         </div>
         <div className={'login__valid'}>
-          <p className={'login__password-warning'}/>
+          {passwordErrorMessage}
         </div>
         <div className={'login__checkbox-wrapper'}>
-          <input className={'login__checkbox'} type={'checkbox'} id={'input-checkbox'}/>
+          <input
+            className={'login__checkbox'}
+            type={'checkbox'}
+            id={'input-checkbox'}
+          />
           <div className={'login__checkbox-mark'}/>
-          <label className={'login__checkbox-label'} htmlFor={'input-checkbox'}>Я согласен получать обновления на
-            почту</label>
+          <label className={'login__checkbox-label'} htmlFor={'input-checkbox'}>
+            Я согласен получать обновления на почту
+          </label>
         </div>
         <Button login onClick={onClick}>
-          <Link to={'/products'} className={'enter-link'}>
           Войти
-          </Link>
         </Button>
       </form>
     </div>
